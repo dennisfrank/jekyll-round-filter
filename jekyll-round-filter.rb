d2a7be924111e0c0845b8fb619db9e23819c157d
Dennis Frank <mail@dennisfrank.info>
2013-12-04 18:12:36 +0100

    Added jekyll-round-filter.rb

---
+++ jekyll-round-filter.rb
@@ -0,0 +1,29 @@
+# Round a float to a given precision in decimal digits
+#
+# input - The number to round
+# precision - Precision in decimal digits (default 0 digits)
+#
+# Returns the rounded number.
+#
+# Usage:
+#
+# {{ float | round: precision }}
+#
+# Examples:
+#
+# {{ 512.4562 | round }}      => 512
+# {{ 512.4562 | round: 2 }}   => 512.46
+#
+# Reference:
+#
+# http://ruby-doc.org/core-1.9.3/Float.html#method-i-round
+
+module Jekyll
+  module RoundFilter
+    def round(input, precision = 0)
+      input.round(precision)
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::RoundFilter)
